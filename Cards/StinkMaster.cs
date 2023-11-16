61e27858aab9ac3c98b3e4288fa710d80dfb4104
Alex Haynes <alexhaynes32@gmail.com>
2023-11-16 16:46:01 -0500

    Initial commit

---
+++ Cards/StinkMaster.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+
+namespace DopeBoys.Cards
+{
+    class StinkMaster : CustomCard
+    {
+
+        public static GameObject toxicObj;
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+            block.forceToAdd = -8f;
+            statModifiers.health = 1.2f;
+            block.cdAdd = 0.25f;
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Edits values on player when card is selected
+
+            // load toxic cloud effect from Toxic cloud card
+            GameObject? toxicCloud = (GameObject)Resources.Load("0 cards/Toxic cloud");
+            toxicObj = toxicCloud.GetComponent<Gun>().objectsToSpawn[0].effect;
+
+            
+            var objToSpawn = new List<GameObject>
+            {
+                toxicObj,
+                toxicObj,
+                toxicObj
+            };
+            block.objectsToSpawn = objToSpawn;
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+            if (block.objectsToSpawn.Contains(toxicObj))    //It should contain it, but just to avoid errors
+            {
+                block.objectsToSpawn.Remove(toxicObj);
+            }
+            else
+            {
+                UnityEngine.Debug.LogWarning("No Toxic Objects Found even though Stink Master was removed");
+            }
+        }
+
+
+        protected override string GetTitle()
+        {
+            return "Stink Master";
+        }
+        protected override string GetDescription()
+        {
+            return "I'm the stink master!\nAdds a noxious cloud to your block";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                 new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Block Cooldown",
+                    amount = "+0.25s",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                  new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Health",
+                    amount = "+20%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+   
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.PoisonGreen;
+        }
+        public override string GetModName()
+        {
+            return DopeBoys.ModInitials;
+        }
+    }
+}
---
+++ DopeBoys.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using DopeBoys.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace DopeBoys
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class DopeBoys : BaseUnityPlugin
+    {
+        private const string ModId = "com.phalex.rounds.DopeBoys";
+        private const string ModName = "DopeBoys";
+        public const string Version = "0.1.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "DB";
+
+        public static DopeBoys instance { get; private set; }
+
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<StinkMaster>();
+        }
+    }
+}
